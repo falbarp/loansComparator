8df3bc99ee537664e3f5e2cde2a6915e0adbd4eb
Fernando Albar <fernandoalbar@gmail.com>
2023-06-05 18:05:52 +0200

    Add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { LoanModule } from './loans/loan.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-
-    MongooseModule.forRoot('mongodb://localhost:27017/loans'),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     LoanModule,
     CommonModule,
     SeedModule,
